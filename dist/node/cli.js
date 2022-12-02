4fe31a82d5b05e8cb83cb60db8763b608e066768
jiangsongyang <2744856707@qq.com>
2022-12-02 10:39:39 +0800

    add dev command

--- dist/node/cli.js
+++ dist/node/cli.js
@@ -1,13 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const cac_1 = require("cac");
+const dev_1 = require("./dev");
+const node_path_1 = require("node:path");
 const version = require("../../package.json").version;
 const cli = (0, cac_1.cac)("tiny-doc").version(version).help();
 cli
     .command("[root]", "start dev server")
     .alias("dev")
     .action(async (root) => {
-    console.log("dev", root);
+    // 添加以下逻辑
+    root = root ? (0, node_path_1.resolve)(root) : process.cwd();
+    const server = await (0, dev_1.createDevServer)(root);
+    await server.listen();
+    server.printUrls();
 });
 cli
     .command("build [root]", "build for production")
--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,4 +1,6 @@
 import { cac } from "cac";
+import { createDevServer } from "./dev";
+import { resolve } from "node:path";
 
 const version = require("../../package.json").version;
 
@@ -8,7 +10,11 @@ cli
   .command("[root]", "start dev server")
   .alias("dev")
   .action(async (root: string) => {
-    console.log("dev", root);
+    // 添加以下逻辑
+    root = root ? resolve(root) : process.cwd();
+    const server = await createDevServer(root);
+    await server.listen();
+    server.printUrls();
   });
 
 cli
